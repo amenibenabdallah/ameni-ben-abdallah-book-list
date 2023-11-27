f2e8245927262efcc693965e4ede7d0f747061bd
amenibenabdallah <benabdallah2ameni@gmail.com>
2023-11-28 00:58:11 +0100

    simple version

--- main.js
+++ main.js
@@ -3,55 +3,97 @@ function MakeBooks(titel, author, category, link, date) {
     obj.titel = titel
     obj.author = author
     obj.category = category
-    obj.read = false
-    obj.date = date
+    
     return obj
 }
 
 function MakeBooksList() {
     var obj = {}
-    obj.bookReaded = 0
-    obj.bookNotReaded = 0
+    
     obj.arrayBooks = []
-    obj.nextBook = {}
-    obj.currentBook = {}
-    obj.lastBook = {}
+   
     obj.add = addBook
-    // obj.getNextBook=getNextBook
-    // obj.finishCurrentBook=finishCurrentBook
+    
+    obj.finishBook=finishBook
     return obj
 }
+
+//methode of the object
+// first methode add book
 var addBook = function (book) {
     this.arrayBooks.push(book)
-    // this.bookNotReaded ++
-    // this.currentBook=getCurrentBooks(this.arrayBooks)
-    // this.nextBook=getTheNextBook(this.arrayBooks)
+    
 }
+var finishBook=function(){
+    
+        booklist1.arrayBooks.shift()
+    }
+
+//clear button functionality
+$('#clear-list').click(function () {
+    
+    $('#list-of-books').empty()
+    $('#list-of-books').append(`
+    
+        <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Category</th>
+            
+        </tr>
+
+    `)
+})
 
 
 var booklist1 = MakeBooksList()
 
 
-
+// new book button functionality
 $('#show-formulaire').click(function () {
     $('.book-formulaire').toggle()
+    $('#add').toggle()  
+})
+$('#add').click(function () {
     $('#add').toggle()
-   
+})
+
+//clear button functionality
+$('#clear-list').click(function () {
+    
+    $('#list-of-books').empty()
+    $('#list-of-books').append(`
+    
+        <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Category</th>
+            
+        </tr>
+
+    `)
+})
+
+//finish book button functionality
+$('#finish').click(function () {
+    booklist1.finishBook
+    display()
 })
 
 
 
+//function used when click add to create new book
 function addItem() {
 
     var newTitle = $('#title').val()
     var newAuthor = $('#author').val()
     var newCategory = $('#category').val()
-    var date = $('#date').val()
-    var newBook = MakeBooks(newTitle, newAuthor, newCategory, date)
+   
+    var newBook = MakeBooks(newTitle, newAuthor, newCategory)
     console.log("hi ", newBook)
     booklist1.add(newBook)
     $('.book-formulaire').hide();
-    $('.book-formulaire').hide(); 
+     
 
 
 }
@@ -68,11 +110,11 @@ function display() {
 <td>${booklist1.arrayBooks[i].titel}</td>
 <td>${booklist1.arrayBooks[i].author}</td>
 <td>${booklist1.arrayBooks[i].category}</td>
-<td>${booklist1.arrayBooks[i].read}</td>
-<td>${booklist1.arrayBooks[i].date}</td>
+
 
 </tr>
 
 `)
     }
 }
+
